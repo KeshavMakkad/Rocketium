2efb4a8a0ee190cd2aa5e659555072c3a8d66c81
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2024-08-01 00:55:18 +0530

    add fetch data functionality

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,11 +1,15 @@
-const express = require("express");
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 const url = "https://microsoftedge.github.io/Demos/json-dummy-data/256KB.json";
 
 const fetchData = async () => {
   const data = await axios.get(url);
-  console.log(data);
+  const dataPath = path.join(__dirname, "../Data/dummyData.json");
+  console.log(path);
+  fs.writeFileSync(dataPath, JSON.stringify(data.data, null, 2));
+  //   console.log(data);
 };
 
 fetchData();
